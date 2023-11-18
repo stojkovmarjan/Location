8b3782c107c1bb3639b1ffddbf2ad8d14498819a
stojkovmarjan <stojkovmarjan@gmail.com>
2023-11-18 21:46:30 +0100

    excuse to excuses

--- LocationApi/DTOs/DummyReport.cs
+++ LocationApi/DTOs/DummyReport.cs
@@ -15,6 +15,6 @@ namespace LocationApi.DTOs
         public string? geozone_id { get; set; } = "test geozone id";
         public string? geozone_name { get; set; } = "test geozone name";
         public string? inzone_time { get; set; } = "122:00:00";
-        public string? excuse_time { get; set; } = "101:00:00";
+        public string? excuses_time { get; set; } = "101:00:00";
     }
 }
