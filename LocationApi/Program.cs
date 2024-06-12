2655006dd7fdf7cebf0562e51f439f9175484117
stojkovmarjan <stojkovmarjan@gmail.com>
2024-06-12 13:08:49 +0200

    //test

--- LocationApi/Program.cs
+++ LocationApi/Program.cs
@@ -5,7 +5,7 @@ using Serilog.Events;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+//test
 //serilog
 Log.Logger = new LoggerConfiguration() // serilog to file from nuget
     .MinimumLevel.Information()
